26b2980acf210a36007820de9263eac4383cda6f
tofik elias <eleltofik@gmail.com>
2023-11-29 14:18:44 +0200

    Show Issue Details

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+interface Props {
+  params: { id: string };
+}
+
+export async function GET(request: NextRequest, { params: { id } }: Props) {
+  const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
+  return NextResponse.json({ issue }, { status: 200 });
+}
