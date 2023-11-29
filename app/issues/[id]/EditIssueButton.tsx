5828c346f2b427890422cc8f38f5c1bfe91b187f
tofik elias <eleltofik@gmail.com>
2023-11-29 17:09:12 +0200

    Refactor: Aplly the SRP

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,17 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Link href={`/issues/${issueId}/edit`}>
+      <Button className="hover:cursor-pointer">
+        <Pencil2Icon />
+        Edit issue
+      </Button>
+    </Link>
+  );
+};
+
+export default EditIssueButton;
