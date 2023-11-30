79261198d35c12f24faa68be1d6bc02079a5621f
tofik elias <eleltofik@gmail.com>
2023-11-30 11:00:15 +0200

    Improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import IssueForm from "../../_components/IssueForms";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "../../_components/IssueFormSkeleton";
+
 interface Props {
   params: { id: string };
 }
 
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  loading: () => <IssueFormSkeleton />,
+  ssr: false,
+});
 const EditIssuePage = async ({ params: { id } }: Props) => {
   const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
   if (!issue) notFound();
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,24 +1,20 @@
 "use client";
-import React, { useState } from "react";
-import { useForm, Controller, FieldValues } from "react-hook-form";
+import { Button, TextField } from "@radix-ui/themes";
 import axios from "axios";
-import { useRouter } from "next/navigation";
-import { TextField, Button } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, FieldValues, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { issueSchema } from "@/app/validationSchemas";
-import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { issueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
+import { z } from "zod";
 
 type IssueFormData = z.infer<typeof issueSchema>;
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
