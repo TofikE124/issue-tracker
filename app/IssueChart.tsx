be61934efa88f8dcd1e2a00073452a332ba27e4d
tofik elias <eleltofik@gmail.com>
2023-12-02 07:28:33 +0200

    Build the Issue Charts component

---
+++ app/IssueChart.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data: { label: string; value: number }[] = [
+    { label: "Open", value: open },
+    { label: "In progress", value: inProgress },
+    { label: "Closed", value: closed },
+  ];
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data}>
+        <XAxis dataKey="label" />
+        <YAxis />
+        <Bar dataKey="value" barSize={60} style={{ fill: "var(--accent-9)" }} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
+import IssueChart from "./IssueChart";
 
 interface Props {
   searchParams: { page: string };
@@ -14,5 +15,10 @@ export default async function Home({ searchParams: { page } }: Props) {
   const closed =
     (await prisma?.issue.count({ where: { status: "CLOSED" } })) || 0;
 
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssueChart open={open} inProgress={inProgress} closed={closed} />
+    </>
+  );
 }
