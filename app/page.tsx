9b28548e8ce208c3e78013c388eaca60bc59e3db
tofik elias <eleltofik@gmail.com>
2023-12-02 07:14:51 +0200

    Build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
 
 interface Props {
   searchParams: { page: string };
 }
 
-export default function Home({ searchParams: { page } }: Props) {
-  return <LatestIssues />;
+export default async function Home({ searchParams: { page } }: Props) {
+  const open = (await prisma?.issue.count({ where: { status: "OPEN" } })) || 0;
+  const inProgress =
+    (await prisma?.issue.count({ where: { status: "IN_PROGRESS" } })) || 0;
+  const closed =
+    (await prisma?.issue.count({ where: { status: "CLOSED" } })) || 0;
+
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
